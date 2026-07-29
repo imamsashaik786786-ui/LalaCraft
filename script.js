86550397a45387ca5fb4a61cdf530f26c3c315d3
Imamsa Shaik <imamsashaik786786@gmail.com>
2026-07-29 13:58:28 +0530

    Added EmailJS Contact Form

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Welcome to LalaCraft");
+emailjs.init({
+  publicKey: "VOm0Ld8_EqxkCNnoK",
+});
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_r7ac2us",
+    "template_m3ib86s",
+    this
+  )
+  .then(function () {
+    alert("✅ Thank you! Your enquiry has been sent successfully.");
+    form.reset();
+  })
+  .catch(function (error) {
+    console.log(error);
+    alert("❌ Failed to send. Please try again.");
+  });
+});
